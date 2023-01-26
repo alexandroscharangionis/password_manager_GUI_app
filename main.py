4bd5db4632eb3ccfeee93e2539b134f8ad6fce3a
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-26 16:16:34 +0200

    added logic to 'save_input' function to check for empty entry fields

--- main.py
+++ main.py
@@ -9,18 +9,21 @@ def save_input():
     username = username_entry.get()
     password = password_entry.get()
 
-    is_ok = messagebox.askokcancel(
-        title=website, message=f"Details entered:\nUsername: {username}\nPassword: {password}\nAre you sure you want to save?")
-    if is_ok:
-        with open("data.txt", "a") as data:
-            data.write(
-                f"WEBSITE: {website} | USERNAME/EMAIL: {username} | PASSWORD: {password}\n")
-        website_entry.delete(0, "end")
-        username_entry.delete(0, "end")
-        password_entry.delete(0, "end")
-        website_entry.focus()
+    if website == '' or username == '' or password == "":
+        messagebox.showerror(title="Oops!",
+                             message="Please don't leave any fields empty.")
     else:
-        pass
+
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"Details entered:\nUsername: {username}\nPassword: {password}\nAre you sure you want to save?")
+        if is_ok:
+            with open("data.txt", "a") as data:
+                data.write(
+                    f"WEBSITE: {website} | USERNAME/EMAIL: {username} | PASSWORD: {password}\n")
+            website_entry.delete(0, "end")
+            username_entry.delete(0, "end")
+            password_entry.delete(0, "end")
+            website_entry.focus()
 
 
 # --------------------------------- UI SETUP ---------------------------------
