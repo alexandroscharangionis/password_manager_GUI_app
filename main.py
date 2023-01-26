090be9af5e526355404784a1195b1ceedc8378d6
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-26 14:47:21 +0200

    added labels to window

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200)
 app_logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=app_logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+username_label = Label(text="Username:")
+password_label = Label(text="Password:")
+
+website_label.grid(column=0, row=1)
+username_label.grid(column=0, row=2)
+password_label.grid(column=0, row=3)
 
 window.mainloop()
